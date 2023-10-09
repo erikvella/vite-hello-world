ff922ca9c037237db8100326d01f520e4f93dea4
erikvella <vella.erik1@gmail.com>
2023-10-09 21:26:17 +0100

    [fix] vite

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+  <script>
+  export default {
+  name: 'Footer'
+  }
+  </script>
+<template>
+  <h1>questo è il footer</h1>
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+  <script>
+  export default {
+  name: 'Header'
+  }
+  </script>
+<template>
+  <h1>Questo è l'header</h1>
+</template>
+
+
+<style>
+
+</style>
